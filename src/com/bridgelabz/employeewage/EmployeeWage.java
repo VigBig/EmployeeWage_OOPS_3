f2bbee73d34d856e6d54a9b5a0a7ac6b54060ae2
VighneshBilgi <vhbilgi@gmail.com>
2022-07-25 11:12:44 +0530

    Added: UC8_Multiple_Companies

--- src/com/bridgelabz/employeewage/EmployeeWage.java
+++ src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,21 +1,52 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Scanner;
+
 class CalculateEmployeeWage{
 
-    private final int WAGE_PER_HOUR = 20;
-    private final int FULL_DAY_HOUR = 8;
-    private final int PART_DAY_HOUR = 4;
-    private final int TOTAL_WORK_DAY = 20;
-    private final int TOTAL_WORK_HOUR = 100;
+    Scanner sc = new Scanner(System.in);
+
+    private String companyName;
+    private int wagePerHour = 0;
+    private int fullDayHour = 0;
+    private int partDayHour = 0;
+    private int totalWorkDay = 0;
+    private int totalWorkHour = 0;
 
     private int dayCount = 1;
     private int hourCount = 0;
     private int totalWage = 0;
     private int dayPresent = 0;
 
+    CalculateEmployeeWage(){
+        System.out.println("Enter Company Name:");
+        String companyName = sc.nextLine();
+        this.companyName = companyName;
+
+        System.out.println("Enter Wage per Hour:");
+        int wagePerHour = sc.nextInt();
+        this.wagePerHour = wagePerHour;
+
+        System.out.println("Enter Hours spent on a Full Day's work:");
+        int fullDayHour = sc.nextInt();
+        this.fullDayHour = fullDayHour;
+
+        System.out.println("Enter Hours spent on a Part Time's work:");
+        int partDayHour = sc.nextInt();
+        this.partDayHour = partDayHour;
+
+        System.out.println("Enter Total No. of Working Days in a Month:");
+        int totalWorkDay = sc.nextInt();
+        this.totalWorkDay = totalWorkDay;
+
+        System.out.println("Enter Total No. of Working Hours in a Month:");
+        int totalWorkHour = sc.nextInt();
+        this.totalWorkHour = totalWorkHour;
+    }
+
     public void totalWage(){
 
-        while (dayCount<=TOTAL_WORK_DAY && hourCount<=TOTAL_WORK_HOUR) {
+        while (dayCount<=totalWorkDay  && hourCount<=totalWorkHour) {
 
             int dailyWage = 0;
             int attendance = (int) (Math.random() * 10) % 3;
@@ -23,14 +54,14 @@ class CalculateEmployeeWage{
             switch (attendance) {
                 case 1:
                     System.out.println("Employee is present Full Time");
-                    hourCount = hourCount + FULL_DAY_HOUR;
-                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    hourCount = hourCount + fullDayHour;
+                    dailyWage = wagePerHour * fullDayHour;
                     dayPresent++;
                     break;
                 case 2:
                     System.out.println("Employee is present Part Time");
-                    hourCount = hourCount + PART_DAY_HOUR;
-                    dailyWage = WAGE_PER_HOUR * PART_DAY_HOUR;
+                    hourCount = hourCount + partDayHour;
+                    dailyWage = wagePerHour * partDayHour;
                     dayPresent++;
                     break;
                 default:
@@ -49,13 +80,26 @@ class CalculateEmployeeWage{
     }
 }
 
+
 public class EmployeeWage {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        CalculateEmployeeWage[] cew;
+
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        CalculateEmployeeWage cew = new CalculateEmployeeWage();
-        cew.totalWage();
+        System.out.println("How may company's details would you like to add ?");
+        int num = sc.nextInt();
+
+        cew = new CalculateEmployeeWage[num];
+
+        for(int i=0; i<num; i++){
+            cew[i] = new CalculateEmployeeWage();
+            cew[i].totalWage();
+        }
+
 
     }
 }
